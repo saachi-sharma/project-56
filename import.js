2c0d0625edee94783feb938c5a349de834812b90
saachi-sharma <87311421+saachi-sharma@users.noreply.github.com>
2021-07-21 13:01:03 +0530

    Add files via upload

---
+++ import.js
@@ -0,0 +1,8 @@
+function spaneelement(){
+    window.alert("this is a span element ");
+    document.getElementById("audio").play();
+}
+function divelement(){
+    window.alert("this is a div element");
+    document.activeElement("audio").pause();
+}
